8ef5561e659e23dbba1a12b337a627d6f5e84274
rishibubna <51851115+rishibubna@users.noreply.github.com>
2019-06-16 01:27:31 +0530

    Machine Learning Assignments 1-3
    Linear Regression
    
    Logistic Regression
    
    Regularisation
    
    One vs all Logistic Regression
    
    Neural Network (Forward Propogation)

---
+++ machine-learning-ex1/ex1/normalEqn.m
@@ -0,0 +1,22 @@
+function [theta] = normalEqn(x,y)
+%NORMALEQN Computes the closed-form solution to linear regression 
+%   NORMALEQN(X,y) computes the closed-form solution to linear 
+%   regression using the normal equations.
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the code to compute the closed form solution
+%               to linear regression and put the result in theta.
+%
+
+% ---------------------- Sample Solution ----------------------
+theta = pinv(x'*x) * x' * y;
+
+
+
+% -------------------------------------------------------------
+
+
+% ============================================================
+
+end
